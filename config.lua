12a00c9470259fe72f76a59ccce9a9a185761c1b
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-02-26 02:25:17 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,13 @@
 Config = {
-    Items = {
-        [1] = {3, 5, "goldbar"},
+    Items = { -- These must be added to the database
+        [1] = {3, 5, "goldbar"}, 
         [2] = {3,5, "rolex"}
     },
     Money = {
         Min = 500,
         Max = 1000
     },
-    PoliceAmount = 0,
+    PoliceAmount = 0, -- Police required
     RobbingTime = 30, -- saniye
     WaitMultiplier = 5,
     ItemOrCash = 50 -- 100 is more cash
